08fccccc8068edecb36dfc01e9fec27ae60803a3
Isaac Dominguez <isaac.dominguez@manomano.com>
2021-05-22 13:36:34 +0200

    feat(PCAP): Module 2

---
+++ Module2/lab1.py
@@ -0,0 +1,27 @@
+print("¡Hola, Mundo!")
+
+# print'gonzon' SyntaxError: invalid syntax
+
+# 1 Instruction per line
+# print('gonzon') print('gonzon') print('gonzon') SyntaxError: invalid syntax
+print('gonzon')
+print('gonzon')
+print('gonzon')
+
+# Scape
+# print("\") SyntaxError: EOL while scanning string literal
+print("\\")
+
+print("La Witsi Witsi Arañar" , "subió" , "a su telaraña.")
+
+# key args
+# end
+print("Mi nombre es", "Python.", end="    ")
+print("Monty Python.")
+
+# sep
+print("Mi", "nombre", "es", "Monty", "Python.", sep="-")
+
+# sep + end
+print("Mi", "nombre", "es", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
